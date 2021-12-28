62dd6df3d2f792bb8423642644a430609437520b
sarthakvish <sarthakvishwakarma6@gmail.com>
2021-12-28 15:37:36 +0530

    django-ses set up

--- email_app/views/sending_views.py
+++ email_app/views/sending_views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.template.loader import render_to_string
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -8,6 +9,9 @@ from email_app.models.user_models import CompanyProfile
 from email_app.serializers import TemplatesSerializer
 from bs4 import BeautifulSoup
 from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from email_app.thread_tasks import EmailThread
 
 
 @api_view(['GET'])
@@ -29,8 +33,27 @@ def getCampaignsSubscriber(request):
                     mail_sending_list.append(subcriber.email)
             unique_send_list = list(set(mail_sending_list))
             print(unique_send_list)
+            get_template_to_send(request.user, "hi all", "",
+                                 settings.DEFAULT_FROM_EMAIL,
+                                 unique_send_list, "email/hyber_dashboard.html", {"user": request.user})
             return Response(unique_send_list, status=status.HTTP_200_OK)
         return Response('You do not have permission to view this record ')
     except ObjectDoesNotExist:
         message = {'detail': 'You are not authorized to view campaign'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Function to make email message dynamically
+
+def get_template_to_send(user, email_subject, text_content, from_email, to, template_path, ctx):
+    html = render_to_string(template_path, {"user": ctx})
+
+    email_message = EmailMultiAlternatives(
+        email_subject,
+        text_content,
+        from_email,
+        to,
+    )
+    email_message.attach_alternative(html, "text/html")
+    EmailThread(email_message).start()
+    return email_message
