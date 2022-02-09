426ad99c3f52267383defd1554829765bd8a912b
username <sarthak@we360.com>
2022-02-09 14:24:36 +0530

    working on thread

--- email_app/thread_tasks.py
+++ email_app/thread_tasks.py
@@ -1,7 +1,8 @@
 import threading
 import time
+from smtplib import SMTPException
 
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, BadHeaderError
 
 
 class EmailThread(threading.Thread):
@@ -13,8 +14,18 @@ class EmailThread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        self.email_message.send()
-        # self.countVar.append(1)
+        try:
+            print(threading.currentThread().getName())
+            self.email_message.send()
+            thread_list = threading.enumerate()
+            print('thread list', len(thread_list))
+            # self.countVar.append(1)
+        except BadHeaderError:  # If mail's Subject is not properly formatted.
+            print('Invalid header found.')
+        except SMTPException as e:  # It will catch other errors related to SMTP.
+            print('There was an error sending an email.' + e)
+        except:  # It will catch All other possible errors.
+            print("Mail Sending Failed!")
 
 
 def send_mail_thread(email_message):
